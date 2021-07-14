0d9ae4a28f1c425a4517abb17d202cd1ca9c8ed7
voyagerdva <mariner-9@yndex.ru>
2021-07-14 20:46:54 +0300

    Remove2LettersByStack

--- Remove2SameLettersByStack/A_stack.py
+++ Remove2SameLettersByStack/A_stack.py
@@ -1,19 +1,10 @@
-# ==================================================
-#
-#   For Lection 13.
-#
-#   asymptotics: O(N)
-#
-# ==================================================
+""" STACK! How it works: 
 
-""" модуль, описывающий структуру данных - стек.
-
->>> clear()
->>> is_empty()
-True
 >>> push(1)
 >>> push(2)
 >>> push(3)
+>>> size()
+3
 >>> is_empty()
 False
 >>> pop()
@@ -30,6 +21,12 @@ False
 1
 >>> is_empty()
 False
+>>> push(1)
+>>> size()
+2
+>>> clear()
+>>> is_empty()
+True
 
 """
 _stack = []
